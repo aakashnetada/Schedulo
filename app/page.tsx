86f82f2aa74b55b76b49154357c852a27fbd85a2
aakashnetada <aakashnetada@gmail.com>
2025-04-03 19:27:03 +0530

    Testing Convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import { Import } from "lucide-react";
-import Image from "next/image";
 import {Button} from "@/components/ui/button";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import Tasks from "@/components/schedulo/tasks";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Todoex</h1>
-      <Button >Hi </Button>
-      <p>Tasks</p>
-      {tasks?.map((task, idx))}
+      <Button>Hi</Button>
+      <Tasks />
     </main>
   );
 }
